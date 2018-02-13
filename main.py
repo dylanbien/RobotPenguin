cf41a36ab0cc7c75cef3f1179974107ffe570875
Rena Yang <rayloom7@gmail.com>
2018-02-13 12:16:52 -0800

    worked on popups

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.graphics import *
 
+leftIsOn = False
 
 # ////////////////////////////////////////////////////////////////
 # //            DECLARE APP CLASS AND SCREENMANAGER             //
@@ -33,98 +34,7 @@ class MyApp(App):
 Builder.load_file('main.kv')
 Window.clearcolor = (0.1, 0.1, 0.1, 1) # (WHITE)
 
-
-# ////////////////////////////////////////////////////////////////
-# //                      GLOBAL VARIABLES                      //
-# //                         CONSTANTS                          //
-# ////////////////////////////////////////////////////////////////
-
-
-blueIsOn = False
-redIsOn = False
-unpaused = True
-blueDirection = 0
-blueSpeed = 0
-redDirection = 0
-redSpeed = 0
-whiteDirection = 0
-whiteSpeed = 500
-
-
-# ////////////////////////////////////////////////////////////////
-# //                       MAIN FUNCTIONS                       //
-# //             SHOULD INTERACT DIRECTLY WITH HARDWARE         //
-# ////////////////////////////////////////////////////////////////
-
-
-def update(color):
-# TURNS ON/OFF BY COLOR CONDITIONALLY
-    # BLUE
-    if color == 'B' or 'ALL':
-        if blueIsOn and unpaused and blueSpeed != 0:
-            blue.run(blueDirection, blueSpeed)
-        else:
-            blue.free()
-    # RED
-    if color == 'R' or 'ALL':
-        if redIsOn and unpaused and redSpeed != 0:
-            red.run(redDirection, redSpeed)
-        else:
-            red.free()
-    # WHITE
-    if color == 'W' or 'ALL':
-        if whiteIsOn and unpaused:
-            white.run(whiteDirection, whiteSpeed)
-        else:
-            white.free()
-
-def stopAll():
-# FREES ALL STEPPERS WITHOUT CHANGING SPEED/DIRECTION VALUES
-    # BLUE
-    blue.free()
-    # RED
-    red.free()
-    # WHITE
-    white.free()
-
-def setSpeed(color, val):
-# SETS 'color' STEPPER SPEED TO 'val'
-    # BLUE
-    if color == 'B':
-        global blueSpeed
-        blueSpeed = val
-    # RED
-    if color == 'R':
-        global redSpeed
-        redSpeed = val
-    # WHITE speed is hardcoded
-
-def setDirection(color):
-# SETS 'color' STEPPER DIRECTION
-    # BLUE
-    if color == 'B':
-        global blueDirection
-        if blueDirection == 0:
-            blueDirection = 1
-        else: #blueDirection == 1
-            blueDirection = 0
-    # RED
-    if color == 'R':
-        global redDirection
-        if redDirection == 0:
-            redDirection = 1
-        else: #redDirection == 1
-            redDirection = 0
-    # WHITE
-    if color == 'W':
-        global whiteDirection
-        if whiteDirection == 0:
-            whiteDirection = 1
-        else: #whiteDirection == 1
-            whiteDirection = 0
-
 def quitAll():
-    stopAll()
     quit()
 
 
@@ -140,112 +50,139 @@ class MyApp(App):
 Builder.load_file('main.kv')
 Window.clearcolor = (0.1, 0.1, 0.1, 1) # (WHITE)
 
-
-# ////////////////////////////////////////////////////////////////
-# //        DEFINE MAINSCREEN CLASS THAT KIVY RECOGNIZES        //
-# //                                                            //
-# //   KIVY UI CAN INTERACT DIRECTLY W/ THE FUNCTIONS DEFINED   //
-# //     CORRESPONDS TO BUTTON/SLIDER/WIDGET "on_release"       //
-# //                                                            //
-# //   SHOULD REFERENCE MAIN FUNCTIONS WITHIN THESE FUNCTIONS   //
-# //      SHOULD NOT INTERACT DIRECTLY WITH THE HARDWARE        //
-# ////////////////////////////////////////////////////////////////
-
-
 class MainScreen(Screen):
 
-# //                  BUTTON/SLIDER ACTIONS                     //
-    def exitProgram(self, instance):
-        quitAll()
+    def exitProgram(self, obj):
+        App.get_running_app().stop()
+        Window.close()
 
-    def sliderAction(self, color, val):
-    # FOR BLUE AND RED SLIDERS
-        setSpeed(color, val)
-        update(color)
-
-    def directionButtonAction(self, color):
-    # FOR BLUE AND RED DIRECTION BUTTONS
-        setDirection(color)
-        update(color)
-
-    def offOnButtonAction(self, color):
-    # FOR BLUE AND RED OFF/ON BUTTONS
-        update(color)
-
-    def pauseUnpauseButtonAction(self):
-    # FOR PAUSE/UNPAUSE BUTTON
-        if unpaused:
-            update('ALL')
-        else:
-            stopAll()
-
-# //                     TOGGLE TEXT                            //
-    def blueToggleText(self, state):
-        global blueIsOn
+    def leftToggleText(self, state):
+        global leftIsOn
         if state == 'down':
-            blueIsOn = True
-            return 'ON'
+            leftIsOn = True
+            return 'IN PROGRESS'
         else: #state == 'normal'
-            blueIsOn = False
-            return 'OFF'
-
-    def redToggleText(self, state):
-        global redIsOn
+            leftIsOn = False
+            return 'TURN LEFT'
+        
+    def upToggleText(self, state):
+        global leftIsOn
         if state == 'down':
-            redIsOn = True
-            return 'ON'
+            leftIsOn = True
+            return 'MOVING...'
         else: #state == 'normal'
-            redIsOn = False
-            return 'OFF'
+            leftIsOn = False
+            return 'MOVE UP'
 
-    def whiteToggleText(self, state):
-        global whiteIsOn
+    def downToggleText(self, state):
+        global leftIsOn
         if state == 'down':
-            whiteIsOn = True
-            return 'ON'
+            leftIsOn = True
+            return 'MOVING...'
         else: #state == 'normal'
-            whiteIsOn = False
-            return 'OFF'
+            leftIsOn = False
+            return 'MOVE DOWN'
 
-    def pauseToggleText(self, state):
-        global unpaused
+    def rightToggleText(self, state):
+        global leftIsOn
         if state == 'down':
-            unpaused = False
-            return 'UNPAUSE'
+            leftIsOn = True
+            return 'TURNING...'
         else: #state == 'normal'
-            unpaused = True
-            return 'PAUSE'
+            leftIsOn = False
+            return 'TURN RIGHT'
 
-# POPUP /////////////////////////////////////////////////
-    def callPopup (self):
-        layout = FloatLayout(size_hint = (0.5, 0.5))
-        popup = Popup(title = 'QUIT SPIROGRAPH',
+# ////////////////////////////////////////////////////////////////
+# //                                                            //
+# //                          POPUPS                            //
+# //                                                            //
+# ////////////////////////////////////////////////////////////////
+
+    def quitPopup (self): # QUIT POPUP
+        quitLay = FloatLayout(size_hint = (0.5, 0.5))
+        quitPop = Popup(title = 'QUIT GAME',
             size_hint = (0.3, 0.23),
             auto_dismiss = True,
             title_size = 30,
             title_align = 'center',
-            content = layout)
+            content = quitLay)
         yesButton = Button(text = 'YES',
             size_hint = (0.46, 0.8),
             font_size = 20,
-            pos = (700, 445))
-
+            pos = (700, 425))
         noButton = Button(text = 'NO',
             size_hint = (0.46, 0.8),
             font_size = 20,
-            pos = (965, 445))
+            pos = (965, 425))
         confirmationLabel = Label(text = 'Are you sure you want to quit?',
-            pos = (685, 515),
+            pos = (685, 487.5),
             font_size = 20)
+        
+        yesButton.bind(on_release = self.exitProgram)
+        noButton.bind(on_release = quitPop.dismiss)
+        
+        quitLay.add_widget(yesButton)
+        quitLay.add_widget(noButton)
+        quitLay.add_widget(confirmationLabel)
+        
+        quitPop.open()
 
-        yesButton.bind(on_press = self.quitAction)
-        noButton.bind(on_press = popup.dismiss)
+    def leftPopup (self): # LEFT POPUP
+        leftLay = FloatLayout(size_hint = (0.5, 0.5))
+        leftPop = Popup(title = 'TURNING...',
+            size_hint = (0.240, 0.73),
+            auto_dismiss = True,
+            title_size = 30,
+            title_align = 'center',
+            pos_hint = { 'x' : 19.5 / Window.width,
+                         'y' : 157 / Window.height},
+            content = leftLay)
 
-        layout.add_widget(yesButton)
-        layout.add_widget(noButton)
-        layout.add_widget(confirmationLabel)
+        leftImage = Image(source = 'CARD_Left.png',
+                          keep_ratio = True,
+                          size_hint = (1.5, 1.945),
+                          pos = (-79, 174.75))
+        leftLay.add_widget(leftImage)
 
-        popup.open()
+        leftPop.open()
+
+    def upPopup (self): # UP POPUP
+        upLay = FloatLayout(size_hint = (0.5, 0.5))
+        upPop = Popup(title = 'MOVING...',
+            size_hint = (0.240, 0.73),
+            auto_dismiss = True,
+            title_size = 30,
+            title_align = 'center',
+            pos_hint = { 'x' : 480 / Window.width,
+                         'y' : 157.5 / Window.height},
+            content = upLay)
+
+        upImage = Image(source = 'CARD_Up.png',
+                          keep_ratio = True,
+                          size_hint = (1.5, 1.945),
+                          pos = (381.75, 174.75))
+        upLay.add_widget(upImage)
+
+        upPop.open()
+
+    def downPopup (self): # DOWN POPUP
+        downLay = FloatLayout(size_hint = (0.5, 0.5))
+        downPop = Popup(title = 'MOVING...',
+            size_hint = (0.240, 0.73),
+            auto_dismiss = True,
+            title_size = 30,
+            title_align = 'center',
+            pos_hint = { 'x' : 180 / Window.width,
+                         'y' : 157.5 / Window.height},
+            content = downLay)
+
+        downImage = Image(source = 'CARD_Down.png',
+                          keep_ratio = True,
+                          size_hint = (1.5, 1.945),
+                          pos = (870, 174.75))
+        downLay.add_widget(downImage)
+
+        downPop.open()
 
 sm.add_widget(MainScreen(name = 'main'))
 
