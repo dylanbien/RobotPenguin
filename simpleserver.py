b04b6622e76650088a2b6dca6f342199b27beb2b
Jonathan Liu <jonathanliu@dpengineering.org>
2018-03-12 09:57:35 -0700

    Update simpleserver.py

--- simpleserver.py
+++ simpleserver.py
@@ -6,7 +6,7 @@ lastMessage = '?'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = (ip.address, 15000)
+server_address = (ip.address, 10100)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
